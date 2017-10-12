576853325ca7049d57b125fb8e78683342a4cb5b
spleendog <spleendog@gmail.com>
2017-10-12 10:57:39 +0100

    ARM Demo

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func CityHandler(res http.ResponseWriter, req *http.Request) {
-	data, _ := json.Marshal("{'cities':'Cambridge','Los Angeles','San Francisco, Amsterdam, Berlin, New York','Tokyo','Paris','London'}")
+	data, _ := json.Marshal("{'cities':'Redwood Shores','Cambridge','Los Angeles','San Francisco, Amsterdam, Berlin, New York','Tokyo','Paris','London'}")
 	res.Header().Set("Content-Type", "application/json; charset=utf-8")
 	res.Write(data)
 }
